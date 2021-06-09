ac774c5156e1f5cf65611eafc6a88db123edf367
Stanley Owen <stanleyowen06@gmail.com>
2021-06-09 20:37:22 +0700

    Improved User Interface (`UI`)

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { IconButton } from '@material-ui/core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons/'
 
 const Navbar = () => {
     return (
-        <div className="">
-            Navbar
+        <div className="navbar">
+            <IconButton>
+                <FontAwesomeIcon icon={faChevronLeft} />
+            </IconButton>
+            <IconButton>
+                <FontAwesomeIcon icon={faChevronRight} />
+            </IconButton>
         </div>
     )
 }
