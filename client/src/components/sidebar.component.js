01afa5422c7578d5d4da1b8d5daf51b9440c54f2
Stanley Owen <stanleyowen06@gmail.com>
2021-06-11 17:20:05 +0700

    Added Active Tabs Attribute

--- client/src/components/sidebar.component.js
+++ client/src/components/sidebar.component.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from '@material-ui/core'
 
 const SideBar = () => {
+    const [active, setActive] = useState('home')
+
+    useEffect(() => {
+        const tab = document.getElementById('tab');
+        [tab.childNodes].forEach(a => {
+            // Get the Inner Text inside div tag
+            if(String(a[0].childNodes[0].innerText).toLowerCase() === active) {
+                console.log('truee')
+                a[0].classList.add('active')
+            }
+        })
+    }, [active])
+
     return (
         <div className="sidebar">
-            <div className="m-10">
-                <Button className="full-width rounded-corner p-10" href="/">Home</Button>
-                <Button className="full-width rounded-corner p-10" href="/search">Search</Button>
+            <div className="m-10" id="tab">
+                <Button className="full-width rounded-corner p-10" id="home" href="/">Home</Button>
+                <Button className="full-width rounded-corner p-10" id="search" href="/search">Search</Button>
             </div>
         </div>
     )
