cdfd2dd650fd75b56f6022a131b98d08fe9bc259
Stanley Owen <stanleyowen06@gmail.com>
2021-06-19 17:48:05 +0700

    Added Music Details in Controller

--- client/src/components/controls.component.js
+++ client/src/components/controls.component.js
@@ -1,7 +1,17 @@
 import React from 'react'
 const Controls = ({ properties }) => {
     return (
-        <div>Music Controller</div>
+        <div className="footer flex" id="footer">
+            <div className="w-30 min-180">
+                <div className="flex">
+                    <img src="https://lh3.googleusercontent.com/_fnSo5pFwGb7QJZL6iOTYkHwSJ9yvA16yKZRHUTDodzKTu3kUFu9apc69J8SlP-Q2HUymWy4TNxK4B9mUhubl01d" />
+                    <div className="center-vert ml-10">
+                        <div className="song-title">Back to December</div>
+                        <div className="author">Taylor Swift</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
