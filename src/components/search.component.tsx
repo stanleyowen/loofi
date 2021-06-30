8f76732c8b4266990f33b40e578f7897cf271570
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-06-30 20:54:17 +0700

    Added Input Field For Search Feature

--- src/components/search.component.tsx
+++ src/components/search.component.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { TextField } from '@material-ui/core'
 
 const Search = ({ properties }: any) => {
+    const [keyword, setKeyword] = useState<string>('')
+
+    useEffect(() => {
+        if(keyword) console.log('Currently Searching', keyword, 'Keywords')
+    }, [keyword])
+
     return (
-        <div>Search Component</div>
+        <div className="m-10">
+            <TextField label="Search" variant="outlined" className="search" value={keyword} onChange={e => setKeyword(e.target.value)} />
+        </div>
     )
 }
 
