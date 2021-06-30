50a53ebe435ba880865e7bf1be5ec6926e17eeb0
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-06-30 10:33:20 +0700

    Optimized Code Base Quality

--- src/components/controls.component.tsx
+++ src/components/controls.component.tsx
@@ -3,28 +3,28 @@ import { SkipNext, SkipPrevious } from '../lib/icons.component'
 
 const Controls = ({ properties }: any) => {
     const [property, setProperty] = useState({
-        playing: false,
         duration: 0,
         progress: 0,
+        playing: false,
         audio: new Audio("https://www.chosic.com/wp-content/uploads/2021/06/Underwater.mp3")
     })
     const handleChange = (a: string, b: any) => setProperty({...property, [a]: b})
 
-    property.audio.onloadeddata = () => handleChange('duration', property.audio.duration)
+    useEffect(() => {
+        property.playing ? property.audio.play() : property.audio.pause()
+    }, [property.playing])
 
     const parseTime = (time: number) => {
         const minutes = Math.floor(time / 60)
         const second = Math.floor(time - (minutes * 60))
         return `${(minutes < 10 ? `0${minutes}` : minutes)}:${(second < 10 ? `0${second}` : second)}`
     }
-    useEffect(() => {
-        if(property.playing) property.audio.play()
-        else property.audio.pause()
-    }, [property.playing])
+
+    property.audio.onloadeddata = () => handleChange('duration', property.audio.duration)
 
     property.audio.ontimeupdate = () => {
-        document.getElementById('current-duration')!.innerText = parseTime(property.audio.currentTime)
         handleChange('progress', (property.audio.currentTime/property.audio.duration)*100)
+        document.getElementById('current-duration')!.innerText = parseTime(property.audio.currentTime)
     }
 
     const triggerAudio = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -50,7 +50,7 @@ const Controls = ({ properties }: any) => {
                 </div>
                 <div className="playback-bar">
                     <div className="progress-time center-align" id="current-duration">00:00</div>
-                    <input type="range" className="progress-bar rounded-corner" id="playback-progress" max="100" value={property.progress} />
+                    <input type="range" className="progress-bar rounded-corner" max="100" value={property.progress} readOnly />
                     <div className="progress-time center-align">{parseTime(property.duration ? property.duration : 0)}</div>
                 </div>
             </div>
