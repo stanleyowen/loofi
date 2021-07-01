e675ca7ec88047853777f219c9f493ad258cc86a
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-07-01 12:25:33 +0700

    Improved Skeleton UI

--- src/components/search.component.tsx
+++ src/components/search.component.tsx
@@ -3,6 +3,7 @@ import { Skeleton } from '@material-ui/lab'
 import { TextField } from '@material-ui/core'
 
 const Search = ({ properties }: any) => {
+    const items:any = []
     const [data, setData] = useState<string>()
     const [keyword, setKeyword] = useState<string>('')
     
@@ -10,23 +11,23 @@ const Search = ({ properties }: any) => {
         if(keyword) console.log('Currently Searching', keyword, 'Keywords')
     }, [keyword])
 
-    function searchData() {
-        return data ? 'data' : 
-            (<div className="m-10">
-                <a className="large-card">
-                    <Skeleton variant="circle" height={200} animation="wave" />
-                    <div className="flex">
-                        <span className="mt-10 w-70"><Skeleton variant="text" animation="wave" /></span>
-                        <span className="w-40"><Skeleton variant="text" animation="wave" /></span>
-                    </div>
-                </a>
-            </div>)
-    }
+    if(keyword)
+        for (let i=0; i<4; i++){
+            items.push(<div className="m-10" key={i}>
+                    <a className="large-card">
+                        <Skeleton variant="circle" height={200} animation="wave" />
+                        <div className="flex">
+                            <span className="mt-10 w-70"><Skeleton variant="text" animation="wave" /></span>
+                            <span className="w-40"><Skeleton variant="text" animation="wave" /></span>
+                        </div>
+                    </a>
+                </div>)
+        }
 
     return (
         <div className="m-10">
             <TextField label="Search" variant="outlined" className="search" value={keyword} onChange={e => setKeyword(e.target.value)} />
-            <div className="mt-30 col-4" id="playlist">{keyword ? searchData() : null}</div>
+            <div className="mt-30 col-4" id="playlist">{items}</div>
         </div>
     )
 }
