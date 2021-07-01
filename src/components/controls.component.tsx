35570671983c9469fe2fb3e4044c4a3bb143bd8e
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-07-01 18:35:13 +0700

    Render Audio Data from Parent Element

--- src/components/controls.component.tsx
+++ src/components/controls.component.tsx
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import { SkipNext, SkipPrevious } from '../lib/icons.component'
 
-const Controls = ({ properties }: any) => {
+const Controls = ({ properties, song }: any) => {
     const [property, setProperty] = useState({
         duration: 0,
         progress: 0,
-        playing: false,
-        audio: new Audio("https://www.chosic.com/wp-content/uploads/2021/06/Underwater.mp3")
+        audio: new Audio(song.audio)
     })
     const handleChange = (a: string, b: any) => setProperty({...property, [a]: b})
 
     useEffect(() => {
-        property.playing ? property.audio.play() : property.audio.pause()
-    }, [property.playing])
+        song.playing ? property.audio.play() : property.audio.pause()
+    }, [song.playing])
 
     const parseTime = (time: number) => {
         const minutes = Math.floor(time / 60)
@@ -28,24 +27,23 @@ const Controls = ({ properties }: any) => {
     }
 
     const triggerAudio = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault()
-        handleChange('playing', !property.playing);
+        e.preventDefault();
         (e.target as Element).classList.toggle('pause')
     }
 
     return (
         <div className="footer flex" id="footer">
             <div className="w-30 flex">
-                <img src="https://lh3.googleusercontent.com/_fnSo5pFwGb7QJZL6iOTYkHwSJ9yvA16yKZRHUTDodzKTu3kUFu9apc69J8SlP-Q2HUymWy4TNxK4B9mUhubl01d" alt="Currently Playing Music" />
+                <img src={song.image} alt="Currently Playing Music" />
                 <div className="center-vert ml-10">
-                    <div className="song-title">Back to December</div>
-                    <div className="author">Taylor Swift</div>
+                    <div className="song-title">{song.title}</div>
+                    <div className="author">{song.author}</div>
                 </div>
             </div>
             <div className="w-40 flex center-flex">
                 <div className="flex center-flex">
                     <button>{SkipPrevious()}</button>
-                    <button className="play-btn mrl-10" onClick={triggerAudio}></button>
+                    <button className={(song.playing ? "pause" : "")+" play-btn mrl-10"} onClick={triggerAudio}></button>
                     <button>{SkipNext()}</button>
                 </div>
                 <div className="playback-bar">
