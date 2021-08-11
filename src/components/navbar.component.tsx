64740020f1ddd843187ca09d4dfe84fdd5f14298
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-08-11 11:51:35 +0700

    Added GitHub Icon

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { IconButton } from '@material-ui/core'
-import { ChevronLeft, ChevronRight } from '../lib/icons.component'
+import { ChevronLeft, ChevronRight, GitHub } from '../lib/icons.component'
 
 const Navbar = ({ properties, handleChange }: any) => {
     const [property, setProperty] = useState({
@@ -15,13 +15,16 @@ const Navbar = ({ properties, handleChange }: any) => {
         })
     }, [properties])
 
-    const goBackward = () => handleChange({ id: 'activeTab', value: properties.previousTab, goBackward: true })
-    const goForward = () => handleChange({ id: 'activeTab', value: properties.nextTab, goForward: true })
+    const goBackward = (e: React.MouseEvent<HTMLElement>) => handleChange({ id: 'activeTab', value: properties.previousTab, goBackward: true })
+    const goForward = (e: React.MouseEvent<HTMLElement>) => handleChange({ id: 'activeTab', value: properties.nextTab, goForward: true })
 
     return (
         <div className="navbar">
             <div className="mrl-10"><IconButton onClick={goBackward} disabled={property.disablePrevious} title="Go Back"><ChevronLeft /></IconButton></div>
             <div className="mrl-10"><IconButton onClick={goForward} disabled={property.disableForward} title="Go Forward"><ChevronRight /></IconButton></div>
+            <div className="mrl-10">
+                <IconButton href="https://github.com/stanleyowen/lofi-player" className="p-10" target="_blank"><GitHub /></IconButton>
+            </div>
         </div>
     )
 }
