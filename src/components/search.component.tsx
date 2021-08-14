64e51fd4717536a99f49c05d3df803717df2737b
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-08-14 21:45:39 +0700

    Added Search Query API

--- src/components/search.component.tsx
+++ src/components/search.component.tsx
@@ -2,30 +2,45 @@ import React, { useState, useEffect } from 'react'
 import { Skeleton } from '@material-ui/lab'
 import { Close } from '../lib/icons.component'
 import { TextField, IconButton } from '@material-ui/core'
+import algoliasearch from 'algoliasearch'
 
 const Search = ({ properties }: any) => {
     const items: any = []
-    // const [data, setData] = useState<string>()
+    const [data, setData] = useState<any>()
     const [keyword, setKeyword] = useState<string>('')
 
+    const client = algoliasearch(String(process.env.REACT_APP_ALGOLIA_ID), String(process.env.REACT_APP_ALGOLIA_API_KEY))
     useEffect(() => {
-        if(keyword) console.log('Currently Searching', keyword, 'Keywords')
+        if(keyword) client.initIndex('music').search(keyword).then(({hits}) => console.log(hits))
     }, [keyword])
 
-    if(keyword)
-        for(let i=0; i<4; i++) {
-            items.push(
-                <div className="m-10" key={i}>
-                    <div className="large-card">
-                        <Skeleton variant="circle" height={200} animation="wave" />
-                        <div className="flex">
-                            <span className="mt-10 w-70"><Skeleton variant="text" animation="wave" /></span>
-                            <span className="w-40"><Skeleton variant="text" animation="wave" /></span>
-                        </div>
-                    </div>
-                </div>
-            )
-        }
+    // if(keyword)
+    //     if(!data || data.length === 0)
+    //         for(let i=0; i<4; i++) {
+    //             items.push(
+    //                 <div className="m-10" key={i}>
+    //                     <div className="large-card">
+    //                         <Skeleton variant="circle" height={200} animation="wave" />
+    //                         <div className="flex">
+    //                             <span className="mt-10 w-70"><Skeleton variant="text" animation="wave" /></span>
+    //                             <span className="w-40"><Skeleton variant="text" animation="wave" /></span>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //             )
+    //         }
+    //     else data.map((music: any, index: any) => {
+    //         items.push(
+    //             <div className="m-10" key={index}>
+    //                 <a className="card flex" href={music.link}>
+    //                     <img src={music.image} alt={music.title} />
+    //                     <p className="m-auto w-50">{music.title}</p>
+    //                     <button className="play-btn m-auto" id={(music.title+music.author).replace(/\s/g, "-")}></button>
+    //                 </a>
+    //             </div>
+    //         )
+    //     })
+    // console.log(data)
 
     const ClearQuery = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault()
