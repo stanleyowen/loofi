efbbd186589a00bdb39f456d2d6cec89d6afe662
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-08-15 15:10:35 +0700

    Preview Data from Search API

--- src/components/search.component.tsx
+++ src/components/search.component.tsx
@@ -11,36 +11,41 @@ const Search = ({ properties }: any) => {
 
     const client = algoliasearch(String(process.env.REACT_APP_ALGOLIA_ID), String(process.env.REACT_APP_ALGOLIA_API_KEY))
     useEffect(() => {
-        if(keyword) client.initIndex('music').search(keyword).then(({hits}) => console.log(hits))
+        if(keyword) client.initIndex('music').search(keyword).then(({hits}) => setData(hits))
     }, [keyword])
 
-    // if(keyword)
-    //     if(!data || data.length === 0)
-    //         for(let i=0; i<4; i++) {
-    //             items.push(
-    //                 <div className="m-10" key={i}>
-    //                     <div className="large-card">
-    //                         <Skeleton variant="circle" height={200} animation="wave" />
-    //                         <div className="flex">
-    //                             <span className="mt-10 w-70"><Skeleton variant="text" animation="wave" /></span>
-    //                             <span className="w-40"><Skeleton variant="text" animation="wave" /></span>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             )
-    //         }
-    //     else data.map((music: any, index: any) => {
-    //         items.push(
-    //             <div className="m-10" key={index}>
-    //                 <a className="card flex" href={music.link}>
-    //                     <img src={music.image} alt={music.title} />
-    //                     <p className="m-auto w-50">{music.title}</p>
-    //                     <button className="play-btn m-auto" id={(music.title+music.author).replace(/\s/g, "-")}></button>
-    //                 </a>
-    //             </div>
-    //         )
-    //     })
-    // console.log(data)
+    if(keyword)
+        if(!data || data.length === 0)
+            for(let i=0; i<4; i++) {
+                items.push(
+                    <div className="m-10" key={i}>
+                        <div className="large-card">
+                            <Skeleton variant="circle" height={200} animation="wave" />
+                            <div className="flex">
+                                <span className="mt-10 w-70"><Skeleton variant="text" animation="wave" /></span>
+                                <span className="w-40"><Skeleton variant="text" animation="wave" /></span>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        else if (data.length > 0)
+            data.map((music: any, index: any) => {
+                items.push(
+                    <div className="m-10" key={index}>
+                        <a className="large-card" href={music.link}>
+                            <img src={music.image} alt={music.title} />
+                            <div className="flex">
+                                <div className="m-auto w-70">
+                                    <h3 className="mt-10">{music.title}</h3>
+                                    <p className="author">{music.author}</p>
+                                </div>
+                                <button className="play-btn m-auto" id={(music.title+music.author).replace(/\s/g, "-")}></button>
+                            </div>
+                        </a>
+                    </div>
+                )
+            })
 
     const ClearQuery = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault()
