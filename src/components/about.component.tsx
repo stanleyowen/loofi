28ecea752f306dd3d57071cd667197a543d37caa
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-08-24 11:58:39 +0700

    Added Application Version

--- src/components/about.component.tsx
+++ src/components/about.component.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { AboutOutline } from '../lib/icons.component'
+import { version } from '../../package.json'
 
 const About = ({ properties }: any) => {
     return (
-        <div className="m-10" id="version">About</div>
+        <div className="m-10" id="version">
+            <div className="flex w-50 card p-15">
+                <div className="flex w-80">
+                    <AboutOutline />
+                    <div className="ml-10">
+                        <p>LoFi Player</p>
+                        <p className="small">Version: {version}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
