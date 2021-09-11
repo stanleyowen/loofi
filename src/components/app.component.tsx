e78dff7c90507fe5f7d71e7fa905e267f8a1acc9
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-09-11 21:03:25 +0700

    refactor: get data in parent element

--- src/components/app.component.tsx
+++ src/components/app.component.tsx
@@ -1,9 +1,14 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import Navbar from './navbar.component'
 import BaseLayout from './base.component'
 import Controls from './controls.component'
+import { initializeApp } from 'firebase/app'
+import { getDatabase, ref, onValue } from 'firebase/database'
+import { Slide, Snackbar } from '@material-ui/core'
+import { Alert } from '@material-ui/lab'
 
 const App = ({ properties, handleChange, config }: any) => {
+    const [data, setData] = useState<any>([])
     const [song, setSong] = useState({
         playing: false,
         title: 'Underwater',
@@ -12,6 +17,22 @@ const App = ({ properties, handleChange, config }: any) => {
         audio: new Audio('https://user-images.githubusercontent.com/69080584/129511300-e88655e9-687f-4d0b-acb4-b32c0fa988cf.mp4')
     })
 
+    useEffect(() => {
+        initializeApp(config)
+        onValue(ref(getDatabase(), 'data-dev/'), (snapshot) => {
+            var rawData = snapshot.val(), index = rawData.length, randIndex
+            while(index !== 0) {
+                randIndex = Math.floor(Math.random() * index)
+                index--
+                [rawData[index], rawData[randIndex]] = [rawData[randIndex], rawData[index]]
+            }
+            setData(rawData)
+        })
+        setTimeout(() =>
+            onValue(ref(getDatabase(), '.info/connected'), (snapshot) => snapshot.val() ? setConnectionState(false) : setConnectionState(true))
+        , 5000)
+    }, []) // eslint-disable-line
+    
     const handleSong = useCallback(a => {
         if(!a.id && !a.value)
             a.audio.getAttribute('src') === song.audio.getAttribute('src') ?
