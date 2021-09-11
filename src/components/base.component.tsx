c2e81b07518be9b8c38179b37b4e4f5e0a9659ce
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-09-11 21:12:36 +0700

    fix build error

--- src/components/base.component.tsx
+++ src/components/base.component.tsx
@@ -7,10 +7,8 @@ const BaseLayout = ({ song, config, properties, songData, handleSong }: any) => 
     return (
         <div className="base">
             { properties.activeTab === 'home' ?
-                    <Home properties={properties} handleSong={handleSong} config={config} song={song} /> :
                     <Home properties={properties} handleSong={handleSong} config={config} song={song} songData={songData} /> :
                     properties.activeTab === 'search' ?
-                        <Search properties={properties} handleSong={handleSong} config={config} /> :
                         <Search properties={properties} handleSong={handleSong} config={config} songData={songData} /> :
                         <About /> }
         </div>
