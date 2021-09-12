04c323e35c4404e9fe1033ac8a51f830e1dff92a
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-09-12 19:53:55 +0700

    Optimize electron.js

--- public/electron.js
+++ public/electron.js
@@ -1,21 +1,22 @@
-const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const path = require("path");
-const isDev = require("electron-is-dev");
+const electron = require("electron")
+const path = require("path")
+const isDev = require('electron-is-dev')
 
-let mainWindow;
+let mainWindow
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
 
 function createWindow() {
-    mainWindow = new BrowserWindow({ width: 900, height: 680 , webPreferences: {nodeIntegration: true}});
-    mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`);
+    mainWindow = new BrowserWindow({ width: 900, height: 680 , webPreferences: { nodeIntegration: true }})
+    mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`)
     mainWindow.setMenuBarVisibility(false)
-    mainWindow.on("closed", () => (mainWindow = null));
+    mainWindow.on("closed", () => (mainWindow = null))
 }
-app.on("ready", createWindow);
+
+app.on("ready", createWindow)
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") { app.quit(); }
-});
+    if (process.platform !== "darwin") app.quit()
+})
 app.on("activate", () => {
-    if (mainWindow === null) { createWindow(); }
-});
+    if (mainWindow === null) createWindow()
+})
