d3ae2cc6425d96a118bd306ebfad2435492350e6
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-09-20 12:17:21 +0700

    Added `enterDelay` and `enterNextDelay` props to `Tooltip` Component

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -21,17 +21,17 @@ const Navbar = ({ properties, handleChange }: any) => {
     return (
         <div className="navbar">
             <div className="m-10-auto">
-                <Tooltip title="Go Back"><span>
-                    <IconButton onClick={goBackward} disabled={property.disablePrevious}><ChevronLeft /></IconButton>
-                </span></Tooltip>
+                <Tooltip title="Go Back" enterDelay={500} enterNextDelay={500}><div>
+                    <IconButton onClick={() => triggerAction('previous')} disabled={property.disablePrevious}><ChevronLeft /></IconButton>
+                </div></Tooltip>
             </div>
             <div className="m-10-auto">
-                <Tooltip title="Go Forward"><span>
-                    <IconButton onClick={goForward} disabled={property.disableForward}><ChevronRight /></IconButton>
-                </span></Tooltip>
+                <Tooltip title="Go Forward" enterDelay={500} enterNextDelay={500}><div>
+                    <IconButton onClick={() => triggerAction('next')} disabled={property.disableForward}><ChevronRight /></IconButton>
+                </div></Tooltip>
             </div>
             <div className="mrl-10">
-                <Tooltip title="View on GitHub" arrow>
+                <Tooltip title="View on GitHub" enterDelay={500} enterNextDelay={500}>
                     <IconButton href="https://github.com/stanleyowen/lofi-player" className="p-10" target="_blank"><GitHub /></IconButton>
                 </Tooltip>
             </div>
