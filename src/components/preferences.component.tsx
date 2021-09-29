09b7432e0b50c921c11051be04803188f551c044
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-09-29 16:38:40 +0700

    Added `Preference` Component

---
+++ src/components/preferences.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const About = () => {
+    return (
+        <div>Preferences</div>
+    )
+}
+
+export default About
--- src/components/settings.component.tsx
+++ src/components/settings.component.tsx
@@ -1,7 +1,38 @@
 import React, { useState } from 'react'
+import { Tab, Tabs } from '@mui/material'
+
+import About from './about.component'
+import Preferences from './preferences.component'
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number,
+    value: number
+}
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props
+    return (
+        <div role="tabpanel" hidden={value !== index} id={`tabpanel-${index}`} {...other} className="w-80">
+            {
+                value === index && (<div>{children}</div>)
+            }
+        </div>
+    )
+}
 
 const Settings = () => {
-    return (<div>Setings</div>)
+    const [tabIndex, setTabIndex] = useState<number>(0)
+    
+    return (
+        <div className="flex-nowrap w-100" id="settings">
+            <Tabs orientation="vertical" value={tabIndex} onChange={(_, index: number) => setTabIndex(index)} className="w-20">
+                <Tab label="Preferences" />
+                <Tab label="About" />
+            </Tabs>
+            <TabPanel value={tabIndex} index={0}><Preferences /></TabPanel>
+            <TabPanel value={tabIndex} index={1}><About /></TabPanel>
+        </div>
+    )
 }
 
 export default Settings
