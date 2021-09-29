edad4b6cc79cbf409133020870e2aeab610eb441
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-09-29 16:21:21 +0700

    Added Settings Component (#120)

--- src/components/base.component.tsx
+++ src/components/base.component.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
+
 import Home from './home.component'
 import Search from './search.component'
-import About from './about.component'
+import Settings from './settings.component'
 
 const BaseLayout = ({ song, config, properties, songData, handleSong }: any) => {
     return (
         <div className="base">
             { properties.activeTab === 'home' ?
-                    <Home properties={properties} handleSong={handleSong} config={config} song={song} songData={songData} /> :
-                    properties.activeTab === 'search' ?
-                        <Search properties={properties} handleSong={handleSong} config={config} songData={songData} /> :
-                        <About /> }
+                <Home properties={properties} handleSong={handleSong} config={config} song={song} songData={songData} /> :
+                properties.activeTab === 'search' ?
+                    <Search properties={properties} handleSong={handleSong} config={config} songData={songData} /> :
+                    <Settings /> }
         </div>
     )
 }
---
+++ src/components/settings.component.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+
+const Settings = () => {
+    return (<div>Setings</div>)
+}
+
+export default Settings
