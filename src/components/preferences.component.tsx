ee16d70843e6d22de1b8c94b71fb9be1a37d0e50
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-10-08 08:05:48 +0700

    Only add `themeURL` to body background when the value is not null or false (#141)

--- src/components/preferences.component.tsx
+++ src/components/preferences.component.tsx
@@ -31,7 +31,7 @@ const About = () => {
     useEffect(() => {
         const themeURL = JSON.parse(localStorage.getItem('theme-session') || `{}`).url
         const backgroundElement = document.getElementById('backdrop-image')
-        if(backgroundElement) backgroundElement.style.background = `url(${themeURL})`
+        if(backgroundElement && themeURL) backgroundElement.style.background = `url(${themeURL})`
     }, [])
 
     return (
