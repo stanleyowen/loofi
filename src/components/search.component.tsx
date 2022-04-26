3e17689a524a3a1187a030482eac1023017c9cb7
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-04-26 19:20:55 +0700

    Display Search Results in Different Category

--- src/components/search.component.tsx
+++ src/components/search.component.tsx
@@ -31,7 +31,7 @@ const Search = ({ songData, HOST_DOMAIN }: any) => {
     if(keyword) {
         if(isFetching)
             for(let i=0; i<4; i++) {
-                items.push(
+                items.music.push(
                     <div className="m-10" key={i}>
                         <div className="large-card">
                             <Skeleton variant="circular" height={200} animation="wave" />
@@ -43,9 +43,9 @@ const Search = ({ songData, HOST_DOMAIN }: any) => {
                     </div>
                 )
             }
-        else if (results?.length > 0)
-            results.map((music: any, index: any) => {
-                return items.push(
+        else if (results?.music?.length > 0)
+            results.music.map((music: any, index: any) => {
+                return items.music.push(
                     <div className="m-10" key={index}>
                         <div className="large-card">
                             <img src={HOST_DOMAIN+music.image} />
@@ -58,9 +58,25 @@ const Search = ({ songData, HOST_DOMAIN }: any) => {
                             </div>
                         </div>
                     </div>
-                )
+                );
             })
-        else if(!isFetching && results?.length === 0) items.push(<div>No Results Found for <b>{keyword}</b></div>)
+        if (results?.author?.length > 0)
+            results.author.map((music: any, index: any) => {
+                return items.author.push(
+                    <div className="m-10" key={index}>
+                        <div className="large-card">
+                            <img src={HOST_DOMAIN+music.image} />
+                            <div className="flex">
+                                <div className="m-auto w-70">
+                                    <h3 className="mt-10">{music.title}</h3>
+                                    <p className="author">{music.author}</p>
+                                </div>
+                                <button className="play-btn m-auto" id={(music.title+music.author).replace(/\s/g, "-")}></button>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })
     }
 
     const ClearQuery = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -75,7 +91,24 @@ const Search = ({ songData, HOST_DOMAIN }: any) => {
                 <TextField label="Search (Artists or Songs)" variant="outlined" className="search" value={keyword} onChange={e => setKeyword(e.target.value)} id="search-query" autoFocus autoComplete="off" />
                 <IconButton className={(keyword ? null : 'none') + " p-10 close-btn"} onClick={ClearQuery} size="large">{Close()}</IconButton>
             </div>
-            <div className="mt-30 col-4" id="playlist">{items}</div>
+            {
+                items.music.length !== 0 ?
+                    (<div>
+                        <div className="mt-30 col-4"><b>Songs</b></div>
+                        <div className="col-4" id="playlist">{items.music}</div>
+                    </div>) : null
+            }
+            {
+                items.author.length !== 0 ?
+                    (<div>
+                        <div className="mt-30 col-4"><b>Artists</b></div>
+                        <div className="col-4" id="playlist">{items.author}</div>
+                    </div>) : null
+            }
+            {
+                keyword && !isFetching && results.music?.length === 0 && results.author?.length === 0 ?
+                    (<div className="mt-30">No Results Found for <b>{keyword}</b></div>) : null
+            }
         </div>
     )
 }
