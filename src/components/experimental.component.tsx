de53576693d4da44afc64196523730ebd2dc5e67
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-04-29 22:07:07 +0700

    Added Warning Alert for Experimental Feature

--- src/components/experimental.component.tsx
+++ src/components/experimental.component.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Switch } from '@mui/material'
+import { Alert, Switch } from '@mui/material'
 
 import { SaveLocation, AutoPlay } from '../lib/icons.component'
 
 // eslint-disable-next-line
 const Experimental = () => {
     return (
-        <div className="m-10" id="version">
+        <div className="p-10" id="version">
+            <Alert severity='warning' className='w-100 border-box experimental-warning'>WARNING: These experimental features are all still under active development and subject to non-backward compatible changes or removal in any future version. Use of these features are not recommended in production environments.</Alert>
             <div className="flex w-100 card p-15 mt-10">
                 <div className="flex">
                     <SaveLocation />
